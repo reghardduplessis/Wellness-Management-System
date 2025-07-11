1cae7ab50b6f396f496c45a38d11ac92427e9613
Chad Clifton <chadcvm@gmail.com>
2025-07-11 17:01:21 +0200

    Add  comments to AuthenticateUtils methods
    Added comments just to explain the AuthenticateUtils

--- web-app/src/AuthenticateUtils.java
+++ web-app/src/AuthenticateUtils.java
@@ -1,5 +1,6 @@
 package utils;
 
+// Import required Java standard libraries
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -8,6 +9,13 @@ import java.util.regex.Pattern;
 public class AuthenticateUtils {
 
     // Hashes the password using SHA-256
+    /*
+            * Hashes a given password using the SHA-256 algorithm.
+     * This is used to store passwords securely in the database.
+            *
+            * @param password The plain text password entered by the user
+     * @return A SHA-256 hashed version of the password in hexadecimal format
+     */
     public static String hashPassword(String password) {
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -23,6 +31,13 @@ public class AuthenticateUtils {
     }
 
     // Validates password strength
+    /*
+     * Checks whether the given password meets strong password criteria.
+     * Criteria: Minimum 8 characters, at least 1 digit, 1 lowercase, 1 uppercase, and 1 special character.
+     *
+     * @param password The password entered by the user
+     * @return true if the password is strong, false otherwise
+     */
     public static boolean isStrongPassword(String password) {
         // Minimum 8 characters, at least 1 uppercase, 1 lowercase, 1 number, 1 special character
         String pattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+=-]).{8,}$";
